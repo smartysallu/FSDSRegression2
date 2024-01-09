d00f1913604cfea3554b907789ab2ec2c00f6eb9
smartysallu <109454250+smartysallu@users.noreply.github.com>
2024-01-09 15:02:05 +0530

    ModelTrainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -49,13 +49,7 @@ class DataIngestion:
             raise CustomException(e,sys)
         
 
-## Run the Data Ingestion
-        
-if __name__=='__main__':
-    obj=DataIngestion()
-    train_data_path,test_data_path=obj.initiate_data_ingestion()
-    data_transformation = DataTransformation()
-    train_arr,test_arr,_=data_transformation.initaite_data_transformation(train_data_path,test_data_path)
+
     
 
 
